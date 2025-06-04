94eef72c296da8e2fdfc57ee7ac0fb9b42b35892
selenamalig <sbmalig@up.edu.ph>
2025-06-04 17:05:34 +0800

    Create page2.tsx

---
+++ project-127/src/app/page2.tsx
@@ -0,0 +1,11 @@
+//page.tsx    Calendar
+import Calendar from "@/components/calendar/Calendar";
+
+export default function SettingsPage() {
+  return (
+    <main className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Calendar</h1>
+      <Calendar />
+    </main>
+  );
+}
